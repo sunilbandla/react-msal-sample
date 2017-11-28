c4a7e5dea32755ed185b430331d32bf759a62dc0
Sunil B <suband@microsoft.com>
2017-11-28 09:00:51 -0800

    Add login with MSAL.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,123 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AuthService from './services/auth.service';
+import GraphService from './services/graph.service';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.authService = new AuthService();
+    this.graphService = new GraphService();
+    this.state = {
+      user: null,
+      userInfo: null,
+      apiCallFailed: false,
+      loginFailed: false
+    };
+  }
+  componentWillMount() {}
+
+  callAPI = () => {
+    this.setState({
+      apiCallFailed: false
+    });
+    this.authService.getToken().then(
+      token => {
+        this.graphService.getUserInfo(token).then(
+          data => {
+            this.setState({
+              userInfo: data
+            });
+          },
+          error => {
+            console.error(error);
+            this.setState({
+              apiCallFailed: true
+            });
+          }
+        );
+      },
+      error => {
+        console.error(error);
+        this.setState({
+          apiCallFailed: true
+        });
+      }
+    );
+  };
+
+  logout = () => {
+    this.authService.logout();
+  };
+
+  login = () => {
+    this.setState({
+      loginFailed: false
+    });
+    this.authService.login().then(
+      user => {
+        if (user) {
+          this.setState({
+            user: user
+          });
+        } else {
+          this.setState({
+            loginFailed: true
+          });
+        }
+      },
+      () => {
+        this.setState({
+          loginFailed: true
+        });
+      }
+    );
+  };
+
   render() {
+    let templates = [];
+    if (this.state.user) {
+      templates.push(
+        <div key="loggedIn">
+          <button onClick={this.callAPI} type="button">
+            Call Graph's /me API
+          </button>
+          <button onClick={this.logout} type="button">
+            Logout
+          </button>
+          <h3>Hello {this.state.user.name}</h3>
+        </div>
+      );
+    } else {
+      templates.push(
+        <div key="loggedIn">
+          <button onClick={this.login} type="button">
+            Login with Microsoft
+          </button>
+        </div>
+      );
+    }
+    if (this.state.userInfo) {
+      templates.push(
+        <pre key="userInfo">{JSON.stringify(this.state.userInfo, null, 4)}</pre>
+      );
+    }
+    if (this.state.loginFailed) {
+      templates.push(<strong key="loginFailed">Login unsuccessful</strong>);
+    }
+    if (this.state.apiCallFailed) {
+      templates.push(
+        <strong key="apiCallFailed">Graph API call unsuccessful</strong>
+      );
+    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React app with MSAL.js</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {templates}
       </div>
     );
   }
---
+++ src/services/auth.service.js
@@ -0,0 +1,54 @@
+import * as Msal from 'msal';
+
+export default class AuthService {
+  constructor() {
+    this.applicationConfig = {
+      clientID: '60e769a3-2b51-4adf-a7e9-b3377270e648',
+      graphScopes: ['user.read']
+    };
+    this.app = new Msal.UserAgentApplication(
+      this.applicationConfig.clientID,
+      '',
+      () => {
+        // callback for login redirect
+      }
+    );
+  }
+  login = () => {
+    return this.app.loginPopup(this.applicationConfig.graphScopes).then(
+      idToken => {
+        const user = this.app.getUser();
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+      () => {
+        return null;
+      }
+    );
+  };
+  logout = () => {
+    this.app.logout();
+  };
+  getToken = () => {
+    return this.app.acquireTokenSilent(this.applicationConfig.graphScopes).then(
+      accessToken => {
+        return accessToken;
+      },
+      error => {
+        return this.app
+          .acquireTokenPopup(this.applicationConfig.graphScopes)
+          .then(
+            accessToken => {
+              return accessToken;
+            },
+            err => {
+              console.error(err);
+            }
+          );
+      }
+    );
+  };
+}
